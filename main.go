21659f2747376dc86fb0899e92f7319b6e4b2325
Igor Starostenko <igor.star@aol.com>
2020-07-05 18:22:33 -0700

    StandardError type

--- main.go
+++ main.go
@@ -5,30 +5,41 @@ import (
 	"os"
 )
 
-type ArgumentError struct {
+type StandardError struct {
 	name        string
 	description string
 }
 
-func (e *ArgumentError) Error() string {
-	return fmt.Sprintf("ArgumentError: (%s) %s", e.name, e.description)
+func (e *StandardError) Error() string {
+	return fmt.Sprintf("Error: (%s) %s", e.name, e.description)
 }
 
 func fetchArg(i int, name string) (string, error) {
 	if i <= 0 {
-		return "", &ArgumentError{name, "Index not specified."}
+		return "", &StandardError{name, "Index not specified."}
 	}
 	if i >= len(os.Args) {
-		return "", &ArgumentError{name, "Missing argument."}
+		return "", &StandardError{name, "Missing argument."}
 	}
 	return os.Args[i], nil
+}
+
+func readFile(fileName string) {
+	fmt.Printf("Reading file: %s", fileName)
 }
 
 func main() {
 	fileName, err := fetchArg(1, "fileName")
 	if err != nil {
 		fmt.Println(err)
-	} else {
-		fmt.Printf("Reading file: %s", fileName)
+		return
 	}
+
+	// outputDir, err := fetchArg(1, "outputDir")
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+
+	readFile(fileName)
 }
